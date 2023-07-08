90898fbc38c16cfe58ff40487b39298a9e7eff6c
Nastya <treninanst@gmail.com>
2023-07-08 19:13:34 +0400

    Added 2ex

---
+++ 2ex/Program.cs
@@ -0,0 +1,37 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+int N = Prompt("Введите первое число");
+int M = Prompt("Введите второе число");
+Console.WriteLine($"{Rang(N,M)}");
+
+int Rang (int m, int n)
+{
+    int sum = 0;
+    if (m == n)
+    {
+        return n;
+    }
+    else if ( m < n)
+    {
+        sum += Rang(m, n - 1 ) + n;
+        return sum;
+    }
+    else 
+    {
+        sum += Rang (n, m - 1) + m;  
+        return sum;
+    }
+}
+
+
+
+
